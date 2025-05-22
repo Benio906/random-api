d1925327c9cee321e5be178e5c6d34d2bb7e340e
Benio906 <Praktyka@impulso.pl>
2025-05-22 09:43:28 +0000

    add app

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
 from flask import flask
-import random
-
 app = Flask(__name__)
-
-quotes = [
-      "zycie to nie bajka kiedy musisz pisac skrypty"
-      "php to rak..."
-      "nienawidze informatyki"
-]
 
 @app.route('/')
 def hello():
-    return random.choice(quotes)
-    
-if __name__ == '__main__':
-    app.run()
+    return "hello"
